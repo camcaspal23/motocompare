ccc6c627b556b15eeaec6706278fe537af15e854
Camilo <camcaspal@gmail.com>
2017-03-05 21:50:03 -0500

    Main activity and Propietario Activity

--- app/src/main/java/com/example/camilo/motocompare/SoyPropietario.java
+++ app/src/main/java/com/example/camilo/motocompare/SoyPropietario.java
@@ -3,11 +3,11 @@ package com.example.camilo.motocompare;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SoyPropietario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_soy_propietario);
     }
 }
