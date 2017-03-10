aa3e83052aca5ffde029f413d05f9adebad8488d
Camilo <camcaspal@gmail.com>
2017-03-09 21:47:06 -0500

    Inicio de Tabbed Activity

--- app/src/main/java/com/example/camilo/motocompare/Evaluacion.java
+++ app/src/main/java/com/example/camilo/motocompare/Evaluacion.java
@@ -3,6 +3,7 @@ package com.example.camilo.motocompare;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Evaluacion extends AppCompatActivity {
 
@@ -12,5 +13,11 @@ public class Evaluacion extends AppCompatActivity {
         setContentView(R.layout.activity_evaluacion);
 
         Intent intent2 = getIntent();
+    }
+
+    public void motoActivity(View view) {
+        Intent intent = new Intent(this, MotoActivity.class);
+        startActivity(intent);
+
     }
 }
